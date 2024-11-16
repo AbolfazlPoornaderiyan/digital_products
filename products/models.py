68e9d66ca0b618afc5bbac335a353f215d3ad73d
Abolfazl Poornaderiyan <abolfazlpoor1461@gmail.com>
2024-11-16 23:53:18 +0330

    complete models.

--- products/models.py
+++ products/models.py
@@ -1,46 +1,45 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
 class Category(models.Model):
-    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
-    title = models.CharField(verbose_name=_('Title'), max_length=50)
-    description = models.TextField(verbose_name=_('Description'), blank=True)
-    avatar = models.ImageField(_('Avatar'), blank=True, upload_to='categories')
-    is_enable = models.BooleanField(_('Is enable'), default=True)
-    created_time = models.DateTimeField(_('Created time'), auto_now_add=True)
-    updated_time = models.DateTimeField(_('Updated time'), auto_now=True)
+    parent = models.ForeignKey('self', verbose_name='parent', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Title', max_length=50)
+    description = models.TextField(verbose_name='Description', blank=True)
+    avatar = models.ImageField('Avatar', blank=True, upload_to='categories/')
+    is_enable = models.BooleanField('Is enable', default=True)
+    created_time = models.DateTimeField('Created time', auto_now_add=True)
+    updated_time = models.DateTimeField('Updated time', auto_now=True)
 
     class Meta:
-        db_table = _('categories')
-        verbose_name = _('Category')
-        verbose_name_plural = _('Categories')
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class Product(models.Model):
-    title = models.CharField(_('Title'), max_length=50)
-    description = models.TextField(_('Description', blank=True))
-    avatar = models.ImageField(_('Avatar'), blank=True, upload_to='products/')
-    is_enable = models.BooleanField(_('Is_enable'), default=False)
-    Categories = models.ManyToManyField('Category', verbose_name=_('Categories'), blank=True)
-    created_time = models.DateTimeField(_('Created time'), auto_now_add=True)
-    updated_time = models.DateTimeField(_('Updated time'), auto_now=True)
+    title = models.CharField('Title', max_length=50)
+    description = models.TextField('Description', blank=True)
+    avatar = models.ImageField('Avatar', blank=True, upload_to='products/')
+    is_enable = models.BooleanField('Is_enable', default=False)
+    Categories = models.ManyToManyField('Category', verbose_name='Categories', blank=True)
+    created_time = models.DateTimeField('Created time', auto_now_add=True)
+    updated_time = models.DateTimeField('Updated time', auto_now=True)
 
     class Meta:
-        db_table = _('products')
-        verbose_name = _('Product')
-        verbose_name_plural = _('Products')
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
 
 class File(models.Model):
-    product = models.ForeignKey('Product', verbose_name=_('Product'), on_delete=models.CASCADE)
-    title = models.CharField(_('Title'), max_length=50)
-    file = models.FileField(_('File'), upload_to='files/')
-    is_enable = models.BooleanField(_('Is enable'), default=False)
-    created_time = models.DateTimeField(_('Created time'), auto_now_add=True)
-    updated_time = models.DateTimeField(_('Updated time'), auto_now=True)
+    product = models.ForeignKey('Product', verbose_name='Product', on_delete=models.CASCADE)
+    title = models.CharField('Title', max_length=50)
+    file = models.FileField('File', upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField('Is enable', default=False)
+    created_time = models.DateTimeField('Created time', auto_now_add=True)
+    updated_time = models.DateTimeField('Updated time', auto_now=True)
 
     class Meta:
-        db_table = _('files')
-        verbose_name = _('File')
-        verbose_name_plural = _('Files')
+        db_table = 'files'
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
